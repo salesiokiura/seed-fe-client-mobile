bc0653be6c498ad155b61af3b64b65b44cfab7b3
youngthug123 <njugunastephen1200@gmail.com>
2023-08-01 11:41:50 +0300

    signup

--- app/src/main/java/ke/ac/tukenya/scit/seedclient/SignupFragment.kt
+++ app/src/main/java/ke/ac/tukenya/scit/seedclient/SignupFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,6 +54,7 @@ class SignupFragment : Fragment() {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
+
                 }
             }
     }
