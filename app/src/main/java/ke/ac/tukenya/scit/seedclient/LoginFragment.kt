aa40a545b3ad7b225ad04f7c9d1cb2ff9584c914
Njorogedennis <njorogedennis6376@gmail.com>
2023-08-18 13:02:17 +0300

    maps,login,landing pages updated

--- app/src/main/java/ke/ac/tukenya/scit/seedclient/LoginFragment.kt
+++ app/src/main/java/ke/ac/tukenya/scit/seedclient/LoginFragment.kt
@@ -1,19 +1,129 @@
 package ke.ac.tukenya.scit.seedclient
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import android.text.method.PasswordTransformationMethod
+import android.text.method.HideReturnsTransformationMethod
+import android.util.Log
+import android.widget.TextView
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
 class LoginFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        auth = FirebaseAuth.getInstance()
+
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val textView: TextView? = view.findViewById(R.id.textView2)
+        textView?.setOnClickListener {
+            findNavController().navigate(R.id.signupFragment)
+        }
+
+        val imageL: ImageView = view.findViewById(R.id.imageView16)  // Assuming the ID is imageView16
+        emailInput = view.findViewById(R.id.tukexample)
+        passwordInput = view.findViewById(R.id.example123_)
+
+        val passwordToggle: ImageView = view.findViewById(R.id.imageView19)
+        passwordToggle.setOnClickListener {
+            if (passwordInput.transformationMethod is PasswordTransformationMethod) {
+                passwordInput.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            } else {
+                passwordInput.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+            passwordInput.setSelection(passwordInput.text.length)
+        }
+
+        imageL.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (isInternetAvailable()) {
+                signIn(email, password)
+            } else {
+                Toast.makeText(context, "No internet connection available.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val myEditText: EditText = view.findViewById(R.id.example123_)
+        myEditText.background = null
+        val myyEditText: EditText = view.findViewById(R.id.tukexample)
+        myyEditText.background = null
+
+        val textView: TextView? = view.findViewById(R.id.textView2)
+        textView?.setOnClickListener {
+            Log.d("Navigation", "Navigating to SignupFragment")
+            findNavController().navigate(R.id.signupFragment)
+        }
+        val textVie1: TextView? = view.findViewById(R.id.forgot_pass)
+        textVie1?.setOnClickListener {
+            Log.d("Navigation", "Navigating to ForgotPasswordFragment")
+            findNavController().navigate(R.id.forgotPasswordFragment)
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Authentication succeeded.", Toast.LENGTH_SHORT).show()
+                    navigateToAnotherFragment()
+                } else {
+                    if (task.exception is FirebaseAuthInvalidUserException) {
+                        // This indicates the user does not exist
+                        Toast.makeText(
+                            context,
+                            "User doesn't exist. Please sign up first.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // This indicates the password is wrong for an existing user
+                        Toast.makeText(
+                            context,
+                            "Incorrect password. Please try again.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        // General error
+                        Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+    private fun navigateToAnotherFragment() {
+        findNavController().navigate(R.id.homeFragment)
     }
 }
--- app/src/main/java/ke/ac/tukenya/scit/seedclient/SignupFragment.kt
+++ app/src/main/java/ke/ac/tukenya/scit/seedclient/SignupFragment.kt
@@ -1,59 +1,160 @@
 package ke.ac.tukenya.scit.seedclient
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var confirmPasswordInput: EditText
+    private lateinit var nameInput: EditText
+    private lateinit var mobileInput: EditText
+    private lateinit var login: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        auth = FirebaseAuth.getInstance()
+
+        val view = inflater.inflate(R.layout.fragment_signup, container, false)
+        val login: TextView = view.findViewById(R.id.textView4)
+
+        val signupButton: ImageView = view.findViewById(R.id.imageView44)
+        emailInput = view.findViewById(R.id.editTextText2)
+        passwordInput = view.findViewById(R.id.editTextTextPassword2)
+        nameInput = view.findViewById(R.id.editTextText)
+        mobileInput = view.findViewById(R.id.editTextPhone)
+        confirmPasswordInput = view.findViewById(R.id.editTextTextPassword5)
+
+        login.setOnClickListener {
+            navigateToAnotherFragment()
+        }
+
+        signupButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            val name = nameInput.text.toString()
+            val mobile = mobileInput.text.toString()
+            val confirmPassword = confirmPasswordInput.text.toString()
+
+            if (isValidEmail(email) && isValidPassword(password, confirmPassword) && isValidName(name) && isValidMobile(mobile)) {
+                signUp(email, password, name, mobile)
+            }
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun navigateToAnotherFragment() {
+        findNavController().navigate(R.id.loginFragment)
+    }
+
+    private fun isValidPassword(password: String, confirmPassword: String): Boolean {
+        if (password != confirmPassword) {
+            Toast.makeText(context, "Passwords do not match.", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (password.length < 6) {
+            Toast.makeText(context, "Password should be at least 6 characters.", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun signUp(email: String, password: String, name: String, mobile: String) {
+        if (isInternetAvailable()) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        saveUserToFirestore(email, name, mobile)
+                    } else {
+                        val errorMessage = when (task.exception) {
+                            is FirebaseAuthUserCollisionException -> "This email is already registered."
+                            is FirebaseAuthWeakPasswordException -> "Please choose a stronger password."
+                            else -> "Registration failed: ${task.exception?.message}"
+                        }
+                        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+                        clearInputFields()
+                    }
                 }
+        } else {
+            Toast.makeText(context, "No internet connection available.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun clearInputFields() {
+        emailInput.text.clear()
+        passwordInput.text.clear()
+        nameInput.text.clear()
+        mobileInput.text.clear()
+    }
+
+    private fun saveUserToFirestore(email: String, name: String, mobile: String) {
+        val user = hashMapOf(
+            "email" to email,
+            "name" to name,
+            "mobile" to mobile
+        )
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users").document(auth.currentUser?.uid!!)
+            .set(user)
+            .addOnSuccessListener {
+                Toast.makeText(context, "Registration successful.", Toast.LENGTH_SHORT).show()
             }
+            .addOnFailureListener {
+                Toast.makeText(context, "Error saving user data: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+    @RequiresApi(Build.VERSION_CODES.FROYO)
+    private fun isValidEmail(email: String): Boolean {
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return true
+        }
+        Toast.makeText(context, "Please enter a valid email address.", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    private fun isValidName(name: String): Boolean {
+        if (name.isNotBlank()) {
+            return true
+        }
+        Toast.makeText(context, "Please enter your name.", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    private fun isValidMobile(mobile: String): Boolean {
+        if (mobile.isNotBlank()) {
+            return true
+        }
+        Toast.makeText(context, "Please enter your mobile number.", Toast.LENGTH_SHORT).show()
+        return false
     }
 }
